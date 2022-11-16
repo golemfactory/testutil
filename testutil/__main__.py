4b0e23353237e8e2f3e23d796302f0787d2d60ec
shadeofblue <blue@golem.network>
2022-11-16 11:04:47 +0100

    limit the scan to the network only

--- testutil/__main__.py
+++ testutil/__main__.py
@@ -1,7 +1,6 @@
 import asyncio
 import click
-import os.path
-from pathlib import Path
+from typing import Optional
 
 from .config import CONFIG
 from .scanner import run_scanner
@@ -13,21 +12,13 @@ def _cli():
 
 
 @_cli.command()
-@click.argument(
-    "payment-network",
-    default="rinkeby",
-    type=str,
-)
-@click.argument(
-    "logfile",
-    default=Path("~/.local/share/yagna/yagna_rCURRENT.log"),
-    type=Path,
-)
-def scan(payment_network: str, logfile: Path):
+@click.argument("payment-network", default="rinkeby", type=click.Choice(CONFIG.keys()))
+@click.option("--address", "-a",type=str, default=None)
+@click.option("--offset", "-o", type=int, default=100, help="Start that many blocks back.")
+@click.option("--verbose", "-v", is_flag=True, default=False, help="Display additional info.")
+def scan(payment_network: str, address: Optional[str], offset: int, verbose: bool):
     config = CONFIG.get(payment_network)
-    logfile = os.path.expanduser(logfile)
-
-    asyncio.run(run_scanner(config, logfile))
+    asyncio.run(run_scanner(config, address, offset, verbose))
 
 
 if __name__ == "__main__":
--- testutil/scanner.py
+++ testutil/scanner.py
@@ -1,36 +1,31 @@
 import asyncio
 from pathlib import Path
+from typing import Optional
 
-import web3.eth
-from web3 import Web3, eth
+from web3 import Web3
 from web3.middleware import geth_poa_middleware
 
 ERC20_ABI_FILE = Path(__file__).parent / "erc20abi.json"
 
 from .eth import wei_to_ether
-from .tail import tailfind
 
 BLOCK_QUERY_INTERVAL = 5.0
 
 
 class Scanner:
-    def __init__(self, config: dict, logfile: str, from_start=False):
+    address: Optional[str] = None
+
+    def __init__(self, config: dict, address: Optional[str], verbose: bool=False):
         self.config = config
-        self.logfile = logfile
-        self.from_start = from_start
-        self.hashes = list()
+        if address:
+            self.address = address.lower()
+        self.verbose = verbose
+
         self.w3 = Web3(Web3.HTTPProvider(self.config["geth_address"]))
         self.contract = self.w3.eth.contract(
             self.config["glm_contract_address"],
             abi=open(ERC20_ABI_FILE, "r").read()
         )
-        self.verbose = False
-
-    async def find_transaction_hashes(self):
-        async for m in tailfind(self.logfile, ".*Send transaction. hash=(.*)", self.from_start):
-            transaction_hash = m.group(1)
-            print("transaction sent: ", transaction_hash)
-            self.hashes.append(transaction_hash)
 
     async def scan_blocks(self, from_block: int, to_block: int):
         if self.verbose:
@@ -38,20 +33,22 @@ class Scanner:
         for e in self.contract.events.Transfer.getLogs(
                 {"fromBlock": from_block, "toBlock": to_block}
         ):
-            hash = e["transactionHash"].hex()
-            if hash in self.hashes or self.verbose:
-                args = e["args"]
-                print("transaction: ", {
+            tx_hash = e["transactionHash"].hex()
+            args = e["args"]
+            if (
+                not self.address
+                or self.address == args["to"].lower()
+                or self.address == args["from"].lower()
+            ):
+                print("transaction: \n", {
                           "from": args["from"],
                           "to": args["to"],
                           "amount": f"{wei_to_ether(args['value']):.16}",
-                          "hash": hash,
+                          "hash": tx_hash,
                 })
 
-    async def run(self):
-        asyncio.create_task(self.find_transaction_hashes())
-
-        previous_block_number = self.w3.eth.get_block_number() - 1
+    async def run(self, offset: int):
+        previous_block_number = self.w3.eth.get_block_number() - offset
 
         while True:
             new_block_number = self.w3.eth.get_block_number()
@@ -62,7 +59,6 @@ class Scanner:
             await asyncio.sleep(BLOCK_QUERY_INTERVAL)
 
 
-async def run_scanner(config, logfile: str):
-    scanner = Scanner(config, logfile)
-
-    await scanner.run()
+async def run_scanner(config, address: Optional[str], offset: int, verbose: bool):
+    scanner = Scanner(config, address, verbose=verbose)
+    await scanner.run(offset)
